2af13ab711cc314b179b64c8f5cf23008e2d26c4
Chong1455 <47151595+Chong1455@users.noreply.github.com>
2020-05-02 19:56:25 +0800

    'second'

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
 const TransactionMiner = require('./app/transaction-miner');
 
+const isDevelopment = process.env.ENV === 'development';
+
 const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
@@ -104,7 +106,7 @@ const syncWithRootState = () => {
 		}
 	});
 };
-
+if (isDevelopment) {
 	const walletFoo = new Wallet();
 	const walletBar = new Wallet();
 
@@ -116,33 +118,33 @@ const syncWithRootState = () => {
 		transactionPool.setTransaction(transaction);
 	};
 
-const walletAction = () => generateWalletTransaction({
-	wallet, recipient: walletFoo.publicKey, amount: 5
-});
+	const walletAction = () => generateWalletTransaction({
+		wallet, recipient: walletFoo.publicKey, amount: 5
+	});
 
-const walletFooAction = () => generateWalletTransaction({
-	wallet: walletFoo, recipient: walletBar.publicKey, amount: 10
-});
+	const walletFooAction = () => generateWalletTransaction({
+		wallet: walletFoo, recipient: walletBar.publicKey, amount: 10
+	});
 
-const walletBarAction = () => generateWalletTransaction({
-	wallet: walletBar, recipient: wallet.publicKey, amount: 15
-});
+	const walletBarAction = () => generateWalletTransaction({
+		wallet: walletBar, recipient: wallet.publicKey, amount: 15
+	});
 
-for (let i=0; i<10; i++) {
-	if (i%3 === 0) {
-		walletAction();
-		walletFooAction();
-	} else if (i%3 === 1) {
-		walletAction();
-		walletBarAction();
-	} else {
-		walletFooAction();
-		walletBarAction();
+	for (let i=0; i<10; i++) {
+		if (i%3 === 0) {
+			walletAction();
+			walletFooAction();
+		} else if (i%3 === 1) {
+			walletAction();
+			walletBarAction();
+		} else {
+			walletFooAction();
+			walletBarAction();
+		}
+
+		transactionMiner.mineTransactions();
 	}
-
-	transactionMiner.mineTransactions();
 }
-
 
 let PEER_PORT;
 
